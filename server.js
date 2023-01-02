dc862247f4b3edc73c107fcf988c8160be328016
Jaxongir1998 <johnolimjonov@gmail.com>
2023-01-02 20:33:21 +0900

    BRR : building express web server

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish kodlari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 Sessionga oid codelar
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+app.get("/hello", function( req, res) {
+    res.end(`<h1>HELLO WORLD</h1>`);
+});
+
+app.get("/gift", function( req, res) {
+    res.end(`<h1>siz sovgalar bolimidasiz</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});

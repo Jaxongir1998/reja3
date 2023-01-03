66cf6f8fa5fbd1067b097afa83a23b00a6beb75e
Jaxongir1998 <johnolimjonov@gmail.com>
2023-01-03 18:39:54 +0900

    fix:modify starting script

--- train.js
+++ train.js
@@ -1,41 +1,25 @@
-//E - Task
+// //E - Task
 
 console.log('train task ishga tushdi! ');
+ const calculator  = (a, b, c) =>{
+ 
 
-let calculator = {};
-
-calculator.kopaytirish = (a, b) => {
-return a * b ;
-};
-calculator.bolish = (a, b) => {
-    return a / b ;
-};
-calculator.qoshish = (a, b) => {
-    return a + b ;
-};
-calculator.ayirish = (a, b) => {
-    return a - b ;
-};
-calculator.default = (a, b) =>{
-    
-    if (a===0 & b===0){
-        console.log("not valid to operate");
+        if (b== "/" && c == 0) {
+      return "not valid to operate";
+   }
+    else if (b== "+") {
+      return a + c;
+      }
+  else if (b == "-") {
+      return a - c;
+       }
+  else if (b == "*"){
+   return a * c;
+   }
+    else if (b == "/"){
+       return a / c;
     }
-};
-
-
-
-const result = calculator.kopaytirish(12,2);
-console.log("result:", result);
-
-const result2 = calculator.bolish(12,2);
-console.log("result2:", result2);
-
-const result3 = calculator.qoshish(12,2);
-console.log("result3:", result3 );
-
-const result4 = calculator.ayirish(12,2);
-console.log("result4:", result4);
-
-const result5 = calculator.default(1,0);
-console.log("result5", result5);
+ }
+ 
+ const result = calculator(15, "-", 2);
+ console.log (result);
